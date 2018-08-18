8346e00e7cf5777791633c96b78f8617108bd36b
Gina Paik <ginapaik@outlook>
2018-08-18 13:19:04 -0400

    update

--- server.js
+++ server.js
@@ -27,4 +27,6 @@ var routes = require("./controllers/burgers_controllers.js");
 
 app.use("/", routes);
 
-app.listen(port);
+app.listen(port, function() {
+    console.log('Friend Finder app is listening on PORT: ' + port);
+  });
